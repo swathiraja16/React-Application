fe0c40c3dda82f5b560736d3f534a59f72982523
Raja Swathi <sr16e@my.fsu.edu>
2018-07-09 18:50:16 -0400

    Introduction to Redux

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
+const store = ConfigureStore();
 
 class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Main />
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
